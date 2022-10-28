5a5dc53da263ca4545773c4424a9acae5abc0208
Anna <michalowska.aneczka@gmail.com>
2022-10-28 19:16:16 +0200

    Delete task-09.js

--- js/task-09.js
+++
@@ -1,9 +0,0 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-const newBackgroundColor = document.querySelector(".color");
-const btnChangeColor = document.querySelector(".change-color");
-btnChangeColor.addEventListener("click", (event) => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  newBackgroundColor.textContent = getRandomHexColor();
-});
