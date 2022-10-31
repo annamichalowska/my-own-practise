01cefdf41486a800b61edafe223ff67e1595e780
Anna <michalowska.aneczka@gmail.com>
2022-10-31 22:49:16 +0100

    Update to_do_list.js

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -3,16 +3,38 @@ const createList = document.querySelector("button[data-create]");
 const startCreateList = document.querySelector("#list");
 const destroyList = document.querySelector("button[data-clear]");
 
-createList.addEventListener("submit", (event) => {
-  if (listInput.value === "") {
-    alert("Enter new note");
-  } else {
-    const itemList = document.createElement("li");
-    startCreateList.appendChild(itemList);
-    itemList.textContent = listInput.value;
-  }
-  listInput.value = "";
-});
+//OOCJA I
+// createList.addEventListener("submit", (event) => {
+//   if (listInput.value === "") {
+//     alert("Enter new note");
+//   } else {
+//     const itemList = document.createElement("li");
+//     startCreateList.appendChild(itemList);
+//     itemList.textContent = listInput.value;
+//   }
+//   listInput.value = "";
+// });
+
+//OPCJA II
+// createList.addEventListener("keypress", (event) => {
+//   if (event.keyCode == 13) {
+//     createList.click();
+//   }
+// });
+
+//OPCJA III
+// createList.addEventListener("keypress", (event) => {
+//   if (event.keyCode == 13) {
+//     if (listInput.value === "") {
+//     alert("Enter new note");
+//   } else {
+//     const itemList = document.createElement("li");
+//     startCreateList.appendChild(itemList);
+//     itemList.textContent = listInput.value;
+//   }
+//   listInput.value = "";
+//   }
+// });
 
 createList.addEventListener("click", (event) => {
   if (listInput.value === "") {
