e7ef42f469d3065765b6f80773f3bd5a0ae898e7
Anna <michalowska.aneczka@gmail.com>
2022-10-31 22:31:08 +0100

    Update to_do_list.js

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -3,6 +3,17 @@ const createList = document.querySelector("button[data-create]");
 const startCreateList = document.querySelector("#list");
 const destroyList = document.querySelector("button[data-clear]");
 
+createList.addEventListener("submit", (event) => {
+  if (listInput.value === "") {
+    alert("Enter new note");
+  } else {
+    const itemList = document.createElement("li");
+    startCreateList.appendChild(itemList);
+    itemList.textContent = listInput.value;
+  }
+  listInput.value = "";
+});
+
 createList.addEventListener("click", (event) => {
   if (listInput.value === "") {
     alert("Enter new note");
